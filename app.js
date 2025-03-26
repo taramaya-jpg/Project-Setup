531d37165ffd0fe2702cfa25ba8f1557d972a095
taramaya-jpg <taramayalimbu55@gmail.com>
2025-03-26 10:50:26 +0600

    Practical 4

--- app.js
+++ app.js
@@ -1,46 +1,57 @@
 // Import required modules
 const express = require('express');
 const path = require('path');
-const dotenv = require('dotenv');
+const dotenv  =  require('dotenv')
 const session = require('express-session');
+const db = require('./config/db');
 const bodyParser = require('body-parser');
-const db = require('./config/db'); // Import database connection
+const indexRoutes = require('./routes/index');
 const authRoutes = require('./routes/authRoutes');
+const homeRoutes = require('./routes/homeRoutes');
+const profileRoutes = require('./routes/profileRoutes');
 
-// Load environment variables
+//load environment variables
 dotenv.config();
+
 
 // Initialize Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'gyeltshenMk',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        maxAge: 60 * 60 * 1000 //session expries after 1 hour
+    }
+}));
+
+
 // Set view engine to EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/components', express.static(path.join(__dirname, 'views/components')));
 
-// Middleware
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Session management
-app.use(
-    session({
-        secret: 'TaraMaya',
-        resave: false,
-        saveUninitialized: true,
-        cookie: { secure: false },
-    })
-);
 
-// Import and use routes
-const indexRoutes = require('./routes/index');
+
+// routes
 app.use('/', indexRoutes);
 app.use('/auth', authRoutes);
+app.use('/', homeRoutes);
+app.use('/', profileRoutes);
 
-// Database test route
+
+//for db connection
 app.get('/db-test', async (req, res) => {
     try {
         const result = await db.one('SELECT NOW() AS current_time');
@@ -49,6 +60,7 @@ app.get('/db-test', async (req, res) => {
         res.status(500).json({ error: 'Database connection failed', details: err.message });
     }
 });
+
 
 // Start server
 app.listen(PORT, () => {
